ddcef4ff21eafdb940bf9c882feb782f7843b493
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-10-22 16:44:50 +0300

    Updates

---
+++ precache-manifest.0f0b178602cc6464fd4b1ac0465d78f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bd3c1c6c8cd8e82464ac6be9b02a94",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "bc5a013129a478016b2b",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.8b637122.chunk.css"
+  },
+  {
+    "revision": "8a90f05b24a498be463d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6c8964e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.6c8964e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5a013129a478016b2b",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.85ac654e.chunk.js"
+  },
+  {
+    "revision": "356906f07e3d724d1528",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.d720b385.js"
+  }
+]);
